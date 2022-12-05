77c44e03eca3f479b504d9706faf82757b515cb8
Salmalisa <salmalisa7342@gmail.com>
2022-12-05 14:44:00 +0600

    curved div done

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import CurvedDiv from '../components/CurvedDiv';
 import Nav from '../shared.js/Nav';
 
 const Main = () => {
   return (
     <div>
       <Nav></Nav>
+      <CurvedDiv></CurvedDiv>
     </div>
   );
 };
